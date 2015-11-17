0d8db61b1a73f48de0243c2d35efd6dc21c42ea4
Jose Moreno <jomoespe@gmail.com>
2015-11-17 08:37:24 +0100

    Update http/2 server impl

--- rest-server.go
+++ rest-server.go
@@ -3,29 +3,38 @@
 package main
 
 import (
+	"fmt"
+	"golang.org/x/net/http2"
 	"io"
 	"log"
 	"net/http"
-	"golang.org/x/net/http2"
 )
 
-func get(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Content-Type", "application/json")
-	io.WriteString(res,
-`{
+func get(w http.ResponseWriter, r *http.Request) {
+	//ShowRequestInfo(r)
+
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w,
+		`{
     "platform": "docker",
     "language": "go",
     "result": "bazinga!"
-}`,
+    }`,
 	)
 }
 
+func ShowRequestInfo(r *http.Request) {
+	fmt.Printf("Method: %s\n", r.Method)
+	fmt.Printf("Protocol: %s\n", r.Proto)
+}
+
 func main() {
-	log.Printf("rest-sample application (HTTP/2) started up successfully")
-	srv := &http.Server {}
+	var srv http.Server
+	//http2.VerboseLogs = true
+	srv.Addr = ":8443"
+	http2.ConfigureServer(&srv, nil)
 	http.HandleFunc("/", get)
-	http2.ConfigureServer(srv, &http2.Server{})
-	err := http.ListenAndServeTLS(":8443", "cert.pem", "key.pem", nil)
+	err := srv.ListenAndServeTLS("cert.pem", "key.pem")
 	if err != nil {
 		log.Fatal(err)
 	}
